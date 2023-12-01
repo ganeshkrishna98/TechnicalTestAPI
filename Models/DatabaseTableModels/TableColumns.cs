5d303e1544fe570bdbf5691bad5c4fcffdb56211
Ganesh Krishna <ganeshkrishna98@gmail.com>
2023-12-01 21:39:55 +0530

    ..

--- Models/DatabaseTableModels/TableColumns.cs
+++ Models/DatabaseTableModels/TableColumns.cs
@@ -2,7 +2,7 @@
 {
     public class TableColumns
     {
-        public const string DocumentColumns = "documentId,documentName,fileName,approvalStatus,authorUserId,authorName,lastModifiedUserId,lastModifiedUserName,lastAccessedUserName,lastAccessedUserId";
+        public const string DocumentsColumns = "documentId,documentName,fileName,approvalStatus,authorUserId,authorName,lastModifiedUserId,lastModifiedUserName,lastAccessedUserName,lastAccessedUserId";
         public const string UserAccountColumns = "userId,userEmail,userSecret,userName,lastAccessTime,lastAccessDevice,lastAccessIp,accountType";
         public const string Authentication = "";
     }
