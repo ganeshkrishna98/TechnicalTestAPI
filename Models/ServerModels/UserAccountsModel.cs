e5af7557bb567519fe2a7c5caeefed3b609990ac
Ganesh Krishna <ganeshkrishna98@gmail.com>
2023-12-02 15:12:15 +0530

    modified useraccounts table

--- Models/ServerModels/UserAccountsModel.cs
+++ Models/ServerModels/UserAccountsModel.cs
@@ -9,11 +9,10 @@ namespace UniversityOfNottinghamAPI.Models.ServerModels
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string userId { get; set; }
         public string userEmail { get; set; }
-        public string userSecret { get; set; }
         public string userName { get; set; }
         public string lastAccessTime { get; set; }
         public string lastAccessDevice { get; set; }
-        public string lastAccessIP { get; set; }
+        public string lastAccessIp { get; set; }
         public string accountType { get; set; }
     }
 }
--- Models/ServiceModels/UserManagement.cs
+++ Models/ServiceModels/UserManagement.cs
@@ -2,13 +2,12 @@
 {
     public class UserManagement
     {
-        public string User_ID { get; set; }
-        public string User_Email { get; set; }
-        public string User_Secret { get; set; }
-        public string User_Name { get; set; }
-        public string Last_Access_Time { get; set; }
-        public string Last_Access_Device { get; set; }
-        public string Last_Access_IP { get; set; }
-        public string Account_Type { get; set; }
+        public string userId { get; set; }
+        public string userEmail { get; set; }
+        public string userName { get; set; }
+        public string lastAccessTime { get; set; }
+        public string lastAccessDevice { get; set; }
+        public string lastAccessIp { get; set; }
+        public string accountType { get; set; }
     }
 }
