4de6288172865255f423c4a356cb7f8c6cc6f7d4
Ganesh Krishna R <ganeshkrishna98@gmail.com>
2023-12-03 23:37:00 +0530

    ..

--- Program.cs
+++ Program.cs
@@ -71,7 +71,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Technical Test V1");
     });
 }
 else
