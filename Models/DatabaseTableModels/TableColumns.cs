2e8a535fc1a14f7affee5b83ecb3dc7689ce8b12
Ganesh Krishna R <ganeshkrishna98@gmail.com>
2023-12-03 22:25:22 +0530

    added all database table constants

--- Models/DatabaseTableModels/TableColumns.cs
+++ Models/DatabaseTableModels/TableColumns.cs
@@ -2,8 +2,11 @@
 {
     public class TableColumns
     {
-        public const string DocumentsColumns = "documentId,documentName,fileName,approvalStatus,authorUserId,authorName,lastModifiedUserId,lastModifiedUserName,lastAccessedUserName,lastAccessedUserId";
-        public const string UserAccountColumns = "userId,userEmail,userName,lastAccessTime,lastAccessDevice,lastAccessIp,accountType";
         public const string AccessLogs = "userId,userEmail,userName,accessTime,accessDate,accessDevice,accessIp,accessedDocumentId,accessedDocumentName,actionPerformed";
+        public const string Devices = "deviceId,deviceName,deviceType,deviceOs,userId,userName,lastAccessedDate,lastAccessedTime";
+        public const string DocumentsColumns = "documentId,documentName,fileName,approvalStatus,authorUserId,authorName,lastModifiedUserId,lastModifiedUserName,lastAccessedUserName,lastAccessedUserId";
+        public const string Notifications = "notificationId,notificationName,notificationContent,createdDate,createdTime,createdUserId,createdUserName";
+        public const string Storages = "storageId,stroageName,createdDate,createdTime,createdUserId,createdUserName";
+        public const string UserAccountColumns = "userId,userEmail,userName,lastAccessTime,lastAccessDevice,lastAccessIp,accountType";
     }
 }
